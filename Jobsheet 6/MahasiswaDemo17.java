55a7d086c29781768d80b18aa4d9da473fded4eb
daffifiros <daffikun10@gmail.com>
2025-03-26 21:33:12 +0700

    Pertanyaan Percobaan 2 No 2

--- Jobsheet 6/MahasiswaDemo17.java
+++ Jobsheet 6/MahasiswaDemo17.java
@@ -1,23 +1,37 @@
+
+import java.util.Scanner;
+
 public class MahasiswaDemo17 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi17 list = new MahasiswaBerprestasi17();
 
-        Mahasiswa17 m1 = new Mahasiswa17("123", "Zidan", "2A", 3.2);
-        Mahasiswa17 m2 = new Mahasiswa17("124", "Ayu", "2A", 3.5);
-        Mahasiswa17 m3 = new Mahasiswa17("125", "Sofi", "2A", 3.1);
-        Mahasiswa17 m4 = new Mahasiswa17("126", "Sita", "2A", 3.9);
-        Mahasiswa17 m5 = new Mahasiswa17("127", "Miki", "2A", 3.7);
-
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
-
-        System.out.println("Data Mahasiswa Sebelum Sorting : ");
+        System.out.println("Data Mahasiswa Sebelum Sorting:");
         list.tampil();
 
-        System.out.println("Data Mahasiswa Sesudah Sorting Berdasarkan IPK (DESC)");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+
+            System.out.print("NIM  : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK  : ");
+            String ipkStr = sc.nextLine().replace(",", ".");
+            double ipk = Double.parseDouble(ipkStr);
+
+            Mahasiswa17 m = new Mahasiswa17(nim, nama, kelas, ipk);
+            list.tambah(m);
+
+            System.out.println("---------------------------------");
+        }
+
+        System.out.println("\nData Mahasiswa Setelah Sorting Berdasarkan IPK (DESC):");
         list.bubbleSort();
         list.tampil();
     }
