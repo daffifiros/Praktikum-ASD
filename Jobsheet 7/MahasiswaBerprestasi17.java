071886979876209df2a7d24231a390f0e3769236
daffifiros <daffikun10@gmail.com>
2025-04-14 15:31:46 +0700

    Pertanyaan Percobaan 2 No 4

--- Jobsheet 7/MahasiswaBerprestasi17.java
+++ Jobsheet 7/MahasiswaBerprestasi17.java
@@ -63,49 +63,48 @@ public class MahasiswaBerprestasi17 {
         }
     }
 
-int sequentialSearching(double cari) {
-    int posisi = -1;
-    for (int j = 0; j < listMhs.length; j++) {
-        if (listMhs[j].ipk == cari) {
-            posisi = j;
-            break;
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data mahasiswa dengan IPK: " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("Data " + x + " tidak ditemukan");
         }
     }
-    return posisi;
-}
 
-void tampilPosisi(double x, int pos) {
-    if (pos != -1) {
-        System.out.println("Data mahasiswa dengan IPK: " + x + " ditemukan pada indeks " + pos);
-    } else {
-        System.out.println("Data " + x + " tidak ditemukan");
-    }
-}
-
-void tampilDataSearch(double x, int pos) {
-    if (pos != -1) {
-        System.out.println("NIM\t: " + listMhs[pos].nim);
-        System.out.println("Nama\t: " + listMhs[pos].nama);
-        System.out.println("Kelas\t: " + listMhs[pos].kelas);
-        System.out.println("IPK\t: " + x);
-    } else {
-        System.out.println("Data mahasiswa dengan IPK: " + x + " tidak ditemukan");
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("NIM\t: " + listMhs[pos].nim);
+            System.out.println("Nama\t: " + listMhs[pos].nama);
+            System.out.println("Kelas\t: " + listMhs[pos].kelas);
+            System.out.println("IPK\t: " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK: " + x + " tidak ditemukan");
         }
-    }   
-int findBinarySearch(double cari, int left, int right) {
+    }
+
+    int findBinarySearch(double cari, int left, int right) {
         int mid;
         if (right >= left) {
             mid = left + (right - left) / 2;
-    
             if (cari == listMhs[mid].ipk) {
-                return mid;
-            } else if (listMhs[mid].ipk > cari) {
+                return (mid);
+            } else if (listMhs[mid].ipk < cari) {
                 return findBinarySearch(cari, left, mid - 1);
             } else {
                 return findBinarySearch(cari, mid + 1, right);
             }
         }
-    
-        return -1; 
+        return -1;
     }
 }
